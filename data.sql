8787f1a8d952b75b857a2079a0f601c7e5e5696d
Gmanlove <emmagman3@gmail.com>
2023-05-03 12:23:40 +0100

    Update tables

--- data.sql
+++ data.sql
@@ -12,3 +12,44 @@ INSERT INTO animals (name, date_of_birth, escape_attempt, neutered, weight_kg, s
 ('Boarmon', '2005-06-07', 7, true, 20.4, NULL),
 ('Blossom', '1998-10-13', 3, true, 17, NULL),
 ('Ditto', '2022-05-14', 4, true, 22, NULL);
+
+
+INSERT INTO owners (full_name,age) VALUES ('Sam Smith',34);
+INSERT INTO owners (full_name,age) VALUES ('Jennifer Orwell',19);
+INSERT INTO owners (full_name,age) VALUES ('Bob',45);
+INSERT INTO owners (full_name,age) VALUES ('Melody Pond',77);
+INSERT INTO owners (full_name,age) VALUES ('Dean Winchester',14);
+INSERT INTO owners (full_name,age) VALUES ('Jodie Whittake',38);
+
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
+
+
+BEGIN WORK;
+update animals SET species_id = 1 WHERE name NOT LIKE '%mon'; 
+update animals SET species_id = 2 WHERE name LIKE '%mon';
+COMMIT WORK;
+
+BEGIN WORK;
+update animals SET owner_id = owners.id 
+FROM owners 
+WHERE animals.name='Agumon' AND owners.full_name='Sam Smith';
+
+update animals SET owner_id = owners.id 
+FROM owners 
+WHERE (owners.full_name='Jennifer Orwell') AND (animals.name='Gabumon' OR animals.name='Pikachu');
+
+update animals SET owner_id = owners.id 
+FROM owners 
+WHERE (owners.full_name='Bob') AND (animals.name='Devimon' OR animals.name='Plantmon');
+
+update animals SET owner_id = owners.id 
+FROM owners 
+WHERE (owners.full_name='Melody Pond') 
+AND (animals.name='Charmander' OR animals.name='Squirtle' OR animals.name='Blossom');
+
+update animals SET owner_id = owners.id 
+FROM owners 
+WHERE (owners.full_name='Dean Winchester') AND (animals.name='Angemon' OR animals.name='Boarmon');
+
+COMMIT WORK;
